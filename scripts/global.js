f1bb7840b3bf168c199f9565ef6e472e0dddabd3
zericsong <zericsong@126.com>
2021-01-01 18:23:23 +0800

    hhh

--- scripts/global.js
+++ scripts/global.js
@@ -30,3 +30,21 @@ function addClass(element,value) {
     }
 }
 
+function highLightPage() {
+    if(!element.getElementByTagName) return false;
+    if(!element.getElementById) return false;
+    let headers = document.getElementByTagName('header');
+    if(headers.length == 0) return false;
+    let nav1 = headers[0].getElementByTagName('nav');
+    if(nav1.length == 0) return false;
+    let links = nav1[0].getElementByTagName('a');
+    let linkurl;
+    for(let i=0;i<links.length;i++) {
+        linkurl = links[i].getAttribute("href");
+        if(window.location.href.indexOf(linkurl) != -1) {
+            links[i].className = "here";
+        }
+    }
+}
+addLoadEvent(highLightPage);
+
